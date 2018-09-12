c15f9a3d604dde7245a9bc7bd0589752be31ab88
Randy Tellez Galán <kronhyx@gmail.com>
2018-09-12 16:47:21 -0400

    add phpunit config

---
+++ Tests/EventSubscriber/DoctrineSubscriberTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kronhyx
+ * Date: 12/09/2018
+ * Time: 4:35 PM
+ */
+
+namespace Kronhyx\TrackerBundle\tests\EventSubscriber;
+
+use Kronhyx\TrackerBundle\EventSubscriber\DoctrineSubscriber;
+
+class DoctrineSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetSubscribedEvents(): void
+    {
+
+    }
+
+    public function testPrePersist(): void
+    {
+
+    }
+}
