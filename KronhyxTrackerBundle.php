52c7b8a301baf7fdaa6e279a646477d198ca5f53
Randy Tellez Galán <kronhyx@gmail.com>
2018-09-12 09:40:04 -0400

    add bundle main file

---
+++ KronhyxTrackerBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kronhyx\TrackerBundle;
+
+/**
+ * Class KronhyxTrackerBundle
+ * @package Kronhyx\TrackerBundle
+ * @author Randy Téllez Galán <kronhyx@gmail.com>
+ */
+class KronhyxTrackerBundle
+{
+}
